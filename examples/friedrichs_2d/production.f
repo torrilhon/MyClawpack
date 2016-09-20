844091bab5fafc9d790365139a781330e620146c
Manuel Torrilhon <torrilhon@rwth-aachen.de>
2016-09-20 19:00:42 +0200

    basic Poisson solve

--- examples/friedrichs_2d/production.f
+++ examples/friedrichs_2d/production.f
@@ -10,7 +10,7 @@ c     # source terms for 2d advaction equations
 c
       tau = 0.1
 c
-      prod(1) = xi*xi
+      prod(1) = 5*xi*xi - 5*yj*yj
       prod(2) = -1.0/tau*q(2)
       prod(3) = -1.0/tau*q(3)
 
--- examples/friedrichs_2d/setplot.py
+++ examples/friedrichs_2d/setplot.py
@@ -37,8 +37,8 @@ def setplot(plotdata):
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = 0
     plotitem.pcolor_cmap = colormaps.red_yellow_blue
-    plotitem.pcolor_cmin = -1.
-    plotitem.pcolor_cmax = 1.
+    plotitem.pcolor_cmin = -1.5
+    plotitem.pcolor_cmax = 1.5
     plotitem.add_colorbar = True
 
     plotitem.amr_celledges_show = [0]  
